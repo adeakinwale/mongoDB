c089efd4cfb2b24e036d5e9b7aa8ae1cfdad1601
Admin <you@example.com>
2019-12-03 05:01:25 +0100

    the increment update operator

--- src/user.js
+++ src/user.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 // blue print of data / collection
 const userSchema = new Schema({
-  name: String
+  name: String,
+  postCount: Number
 });
 
 // creates user collection using the defined userSchema
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -25,12 +25,27 @@ describe("Updating records", () => {
   });
 
   it("A model instance can update", done => {
-    assertName(joe.updateOne({ name: "Alex" }), done);
+    assertName(joe.update({ name: "Alex" }), done);
   });
+
+  it("A model class can update", done => {
+    assertName(User.update({ name: "Joe" }, { name: "Alex" }), done);
+  });
+
   it("A model class can update one record", done => {
     assertName(User.findOneAndUpdate({ name: "Joe" }, { name: "Alex" }), done);
   });
+
   it("A model class can find a record with an ID and update", done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  it("A user can have their postcount incremented by 1", done => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 10 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        assert(user.postCount === 10);
+        done();
+      });
   });
 });
