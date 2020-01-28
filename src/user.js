21fabb18a73080ac2f6921ef0fd8bd2be134b7b8
Admin <you@example.com>
2020-01-28 09:53:12 +0100

    association test

--- src/user.js
+++ src/user.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
+const PostSchema = require("./post");
 const Schema = mongoose.Schema;
 
 // blue print of data / collection
 const userSchema = new Schema({
-  name: String,
-  postCount: Number
+  name: {
+    type: String,
+    validate: {
+      validator: name => name.length > 2,
+      message: "Name must be longer than 2 characters."
+    },
+    required: [true, "Name is required."]
+  },
+
+  posts: [PostSchema],
+  likes: Number
+});
+
+userSchema.virtual("postCount").get(function() {
+  return this.posts.length;
 });
 
 // creates user collection using the defined userSchema
